5e947a428bb5854f46ec93b39c883e4fcca88050
MattGogan <55172084+MattGogan@users.noreply.github.com>
2020-12-30 10:18:34 -0500

    Initial Commit

---
+++ js/utilities.js
@@ -0,0 +1,74 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+var buffer = document.getElementById("divBetweenNavAndContent");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    buffer.classList.add("stickybuffer");
+  } else {
+    navbar.classList.remove("sticky");
+    buffer.classList.remove("stickybuffer");
+  }
+}
+
+
+
+
+var onlyDoes = 0;
+var onlyDoes = [
+  "Advanced Filtration.",
+  "Trusted Safety.",
+  "HEPA Specialists.",
+  "Reliable.",
+  "Carbon Filtrationists.",
+  "Clean Air.",
+  "FilterHub."
+
+]
+cycleOnlyDoes(0);
+
+function cycleOnlyDoes(j){
+
+  
+  var label = document.getElementById("lblOnlyDoes");
+  console.log(label.classList);
+
+  
+  label.classList.remove("w3-animate-bottom");
+  
+  label.classList.add("fade-out-very-fast");
+
+  setTimeout(function(){
+    void label.offsetWidth;
+    label.innerHTML = onlyDoes[j];
+    label.classList.add("w3-animate-bottom");
+    label.classList.remove("fade-out-very-fast");
+    
+      setTimeout(function(){ 
+        
+        
+        cycleOnlyDoes(nextOnlyDoes); 
+        currCycle = nextOnlyDoes;
+      }, 950);
+  }, 950);
+  
+
+
+  nextOnlyDoes = j+1;
+  if(j>onlyDoes.length-2){
+      nextOnlyDoes=0;
+  }
+
+
+  
+}
+
+
